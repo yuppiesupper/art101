27ff8edb4ac7dfb5c764cf88c95c9d9f3d3449cb
Teia Ziegelman <tziegelm@ucsc.edu>
2023-10-26 17:38:13 -0700

    lab 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -19,3 +19,16 @@ function main() {
 
 // let's get this party started
 main();
+
+//Go Fish!
+// Load players with 7 cards
+	// Randomly load cards from the deck
+		// Each card delt is removed from the deck
+// first player clicks card in hand wanting to be matched
+	// Check if the other player has a matching card
+      // If yes: give to player 
+      // If no: “go fish” message
+        // Allow player to draw from random pile
+// other player’s turn
+// check for win condition
+  // When all cards in possession are pairs, player with most pairs wins
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -5,6 +5,52 @@
 // Constants
 
 // Functions
+/**
+ * Author:    Teia Ziegelman
+ * Created:   10.24.2023
+ * 
+ * (c) Copyright ART101 
+ **/
+
+
+// Define Variables
+make = "ford";
+model = "mustang";
+color = "red"
+year = 2006
+
+// calculate
+age = 2023 - year;
+
+// output
+document.writeln("make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + " years<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
