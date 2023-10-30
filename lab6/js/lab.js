a4a055dacde36d626d804fd25581055431438142
Teia Ziegelman <tziegelm@ucsc.edu>
2023-10-29 21:29:56 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - objects lab
+// Author: Teia
+// Date: 10/27/23
 
-// Constants
+// Define Variables
+var myTransport = ["Mustang", "bike", "bus"];
 
-// Functions
+// Object for my main ride
+myMainRide = {
+  make: "Ford",
+  model: "Mustang",
+  color: "red",
+  year: 2006,
+  age: function() {
+    return 2023 - this.year;
+  }
+}
+// output
+document.writeln("Kinds of transportation I use: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
