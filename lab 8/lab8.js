6ccf588585fcca288710e9506195187418865ecc
Teia Ziegelman <tziegelm@ucsc.edu>
2023-11-05 20:02:31 -0800

    lab 8 initial test

---
+++ lab 8/lab8.js
@@ -0,0 +1,14 @@
+// index.js - Lab 8 JS
+// Author: Teia Ziegelman
+// Date: 11/4/23
+
+// Variables
+var myArray = [8, 16, 24, 21, 14, 20, 22]
+
+// Functions
+function add2(x){
+    var results = (x + 2)
+    return results;
+}
+// Print function:
+document.writeln(add2(myArray))
