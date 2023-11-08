6c353a801d377b400b22cd5cd3f849b47eb07d77
Teia Ziegelman <tziegelm@ucsc.edu>
2023-11-07 20:06:27 -0800

    update

---
+++ lab9/lab9.js
@@ -0,0 +1,26 @@
+// index.js - Lab 9 JS
+// Author: Teia Ziegelman
+// Date: 11/7/23
+
+//add button to output
+$("#output").append("<br><button id='dosmth'>click me!</button>");
+$("#output").append("<br><button id='dosmthelse'>click me!</button>");
+
+// add word after H3
+$("h3").append(" FUN!!");
+
+//BUTTON FUNCTIONS
+//button 1
+function myButton() {
+    $("#output").html("boo!")
+};
+
+$("#dosmth").click(myButton);
+
+//button 2
+function myButton2() {
+    $("#output").html("you're safe")
+};
+
+$("#dosmthelse").click(myButton2)
+
