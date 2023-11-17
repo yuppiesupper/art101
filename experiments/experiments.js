9b9eae9201e7c0d50fc5ed2a21ad4406eaf47915
Teia Ziegelman <tziegelm@ucsc.edu>
2023-11-16 19:40:44 -0800

    lab 10 server test

---
+++ experiments/experiments.js
@@ -0,0 +1,8 @@
+//experiment js!
+// author: Teia Ziegelman
+// date: 11/15/23
+
+$("#my-button").click(function(){
+    var name = prompt("Name please:");
+    $("#title").html("Hello, "+ name + "!");
+});
--- lab10/lab10.js
+++ lab10/lab10.js
@@ -7,9 +7,9 @@
    Date: 2023
 */
 function generateRandomText() {
-    const text = "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.";
+    const text = "meow meow mow meow mrrp prrp mew meow meeeoow meow meowww mow mrrp mow meow meow mew mowwww meow";
     const min = 3;
-    const max = 100;
+    const max = 50;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
 
     // Get a random starting index to slice the Lorem Ipsum text
@@ -25,4 +25,9 @@ $("#make-convo").click(function(){
     const newText = generateRandomText();
     // append a new div to our output div
     $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+  });
+  
+// reload when "Shut it!" button is clicked
+  $("#refresh").click(function(){
+    location.reload();
   });
