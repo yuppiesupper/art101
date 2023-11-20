726e34a83b06a539e5514ecd7723182ddf54582c
Teia Ziegelman <tziegelm@ucsc.edu>
2023-11-19 18:08:20 -0800

    lab11

---
+++ lab11/lab11.js
@@ -0,0 +1,27 @@
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Teia Ziegelman
+   Date: 2023
+*/
+
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+  } 
+
+  // click listener for button
+$("#submit").click(function(){
+    // get value of input field
+const userName = $("#user-name").val();
+});
+
+// now let's sort it
+userNameSorted = sortString(userName);  
+
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
