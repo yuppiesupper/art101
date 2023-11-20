900f5294672cf8052b2ff564395a7a01c366a684
Teia Ziegelman <tziegelm@ucsc.edu>
2023-11-19 19:32:28 -0800

    second push

--- lab11/lab11.js
+++ lab11/lab11.js
@@ -15,13 +15,8 @@ function sortString(inputString) {
   } 
 
   // click listener for button
-$("#submit").click(function(){
-    // get value of input field
-const userName = $("#user-name").val();
+$("#submit").click(function(){ 
+    const userName = $("#user-name").val();
+    userNameSorted = sortString(userName); 
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>'); 
 });
-
-// now let's sort it
-userNameSorted = sortString(userName);  
-
-// append a new div to our output div
-$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
