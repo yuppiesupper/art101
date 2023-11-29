658c050a7e3cdc1fc0913c2626893e991b256153
Teia Ziegelman <tziegelm@ucsc.edu>
2023-11-28 18:08:55 -0800

    lab8 debug

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -2,39 +2,24 @@
 // Author: Teia Ziegelman
 // Date: 11/4/23
 
-// Variables
-var myArray = [8, 16, 24, 21, 14, 20, 22]
-
-// Functions
-function add2(x){
-    var results = (x + 2)
-    return results;
-}
-
-// try out wes's code?
-function firstThing(test) {
-    console.log(test + "one")
-}
-
-function secondThing(test) {
-    console.log(test + "two")
-}
-
-function thirdThing(test) {
-    console.log(test + "three!")
-}
-
-firstThing("TEST 1: ");
-secondThing("TEST 2: ");
-thirdThing("TEST 3: ");
-
-// one two three popping up after eachother?
-setTimeout(function() {
-    firstThing("one?");
-}, 0);
-setTimeout(function() {
-    secondThing("two?");
-}, 4000);
-setTimeout(function() {
-    thirdThing("three?");
-}, 6000);
+function printAfterDelay(message, delay, callback) {
+    setTimeout(function () {
+      console.log(message);
+      if (callback) {
+        callback();
+      }
+    }, delay);
+  }
+  
+  function seriesOfStrings() {
+    printAfterDelay("I am first", 0, function () {
+      printAfterDelay("I am second", 2000, function () {
+        printAfterDelay("I am third", 2000);
+      });
+    });
+  }
+  
+  // Start the sequence
+  seriesOfStrings();
+  
+  
